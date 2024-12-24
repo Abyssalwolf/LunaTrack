1418b5ee47b14e466050be0ae639095ac988809e
Sebin Thomas <sebinthomas299@gmail.com>
2024-12-24 23:29:32 +0530

    Implementing a landing page

--- LunaTrack/settings.py
+++ LunaTrack/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'landing',
+    'tailwind',
+    'theme',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +69,11 @@ TEMPLATES = [
         },
     },
 ]
+
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+
+
+TAILWIND_APP_NAME = 'theme'
 
 WSGI_APPLICATION = 'LunaTrack.wsgi.application'
 
@@ -116,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "theme/static",
+    BASE_DIR / "landing/static",]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'primary-pink': '#F28C8C',
+          'secondary-white': '#FFFFFF',
+          'light-lavender': '#D8BFD8',
+          'header-bg': '#D8BFD8',
+          'features-bg': '#FFF8F0',
+          'footer-bg': '#2D2D2D',
+          'dark-purple': '#333333',
+          'primary-text': '#333333',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
+//The login buttons are non existent. When you give an answer cross check with the code you gave above to implement
